cf696c3c8e943978849dabb5635d0f80cd3e8d52
DanielleSilva01 <90563119+DanielleSilva01@users.noreply.github.com>
2023-01-14 12:34:46 -0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+function finalizar(){
+    prompt("Certo, espero que não tenha ficado nenhuma dúvida. Até a próxima, volte sempre! Estarei aqui te esperando :) :)")
+    window.location.href = "https://www.google.com/";
+}
+
+function entrar(){
+    var nome = document.getElementById('nome');
+    var texto = prompt('Para começar, escreva seu nome');
+
+    nome.innerHTML = texto;
+}
